a6efa9ba6ed6e79752fcf0ec70668e9bf1912352
Denis Bikmaev <dbikmaev2003@gmail.com>
2024-04-28 19:46:27 +0300

    Task 20

--- src/main/java/com/example/JavaPatternsTask14/services/PhoneService.java
+++ src/main/java/com/example/JavaPatternsTask14/services/PhoneService.java
@@ -4,14 +4,10 @@ import com.example.JavaPatternsTask14.models.Manufacture;
 import com.example.JavaPatternsTask14.models.Phone;
 import com.example.JavaPatternsTask14.repositories.PhoneRepo;
 import jakarta.annotation.PostConstruct;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -27,19 +23,9 @@ public class PhoneService {
     @PostConstruct
     void init() {
         session = sessionFactory.openSession();
-//        Phone phone = new Phone();
-//        phone.setName("Sony Ericson");
-//        phone.setCreationYear(2005);
-//
-//        var transaction = session.beginTransaction();
-//        session.saveOrUpdate(phone);
-//        transaction.commit();
     }
 
     public Manufacture getManufactureByPhone(Long phoneId) {
-//        return session.createQuery("from Phone where id =: id", Phone.class)
-//                .setParameter("id", phoneId).getSingleResult().getManufacture();
-
         log.info("Get manufacture by phone");
         return phoneRepository.findById(phoneId).orElseThrow().getManufacture();
     }
@@ -68,13 +54,5 @@ public class PhoneService {
     public List<Phone> sortPhones(String column) {
         log.info("Sort phones by order {}", column);
         return phoneRepository.findAll(Sort.by(Sort.Direction.ASC, column));
-//        CriteriaBuilder builder = session.getCriteriaBuilder();
-//        CriteriaQuery<Phone> phoneCriteriaQuery =
-//                builder.createQuery(Phone.class);
-//        Root<Phone> root = phoneCriteriaQuery.from(Phone.class);
-//
-//        phoneCriteriaQuery.select(root).orderBy(builder.asc(root.get(column)));
-//        Query<Phone> query = session.createQuery(phoneCriteriaQuery);
-//        return query.getResultList();
     }
 }
